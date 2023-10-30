3a6e9b346011f952bd99ae791e447775e758f6d5
Blazej Leman <blazejleman789@gmail.com>
2023-10-30 23:11:36 +0100

    add TrafficJam()

--- enumy.cpp
+++ enumy.cpp
@@ -12,6 +12,28 @@ enum class TrafficLights {
     red
 };
 
+void stop() {
+    std::cout << "stop" << std::endl;
+}
+
+void go() {
+    std::cout << "go" << std::endl;
+}
+
+TrafficLights getColor() {
+    return TrafficLights::red;
+}
+
+void TrafficJam() {
+    auto TrafficColor = getColor();
+    if(TrafficColor == TrafficLights::red) {
+        stop();
+    } else {
+        go();
+    }
+}
+
 int main() {
+    TrafficJam();
     return 0;
 }
